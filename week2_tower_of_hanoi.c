1cd99bba2555e1fb7610db2af31ada9db8634661
saraalmemar <141176556+saraalmemar@users.noreply.github.com>
2026-01-05 22:50:35 +0300

    Implement Tower of Hanoi solution in C

---
+++ week2_tower_of_hanoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function to solve Tower of Hanoi
+void hanoi(int n, char from, char to, char aux) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", from, to);
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);
+    printf("Move disk %d from %c to %c\n", n, from, to);
+    hanoi(n - 1, aux, to, from);
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    printf("\nSteps to solve Tower of Hanoi:\n");
+    hanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
